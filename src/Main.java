918a84b34808e61c671ff4fa8d81038f97cbe817
yntymakdev <kubanychevyntymak21@gmail.com>
2025-01-31 09:20:32 +0600

    learned type primitive and refernecce type also scanner

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age: ");
 
+        int  age = scanner.nextInt();
+        System.out.println("Hello"+ name);
+        System.out.println("You are "+ age + " years old");
+        scanner.close();
     }
 }
+
+
